0eef23f35e5be24782985d26958656142f95d260
Brian Woodward <brian.woodward@gmail.com>
2015-08-10 20:15:22 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * ask-for-github-auth <https://github.com/doowb/ask-for-github-auth>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
